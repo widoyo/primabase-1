9c0aeb47f4540d12d81d823d0be5da744210a05e
dhnchrs <37863926+dhnchrs@users.noreply.github.com>
2018-05-22 10:34:21 +0700

    Add files via upload

---
+++ templates/homeUser.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>
+		Home
+		</title>
+	</head>
+	<body>
+		<h1>Home</h1>
+		Selamat datang, <?php echo $_SESSION['username']; ?><br/>
+		<a href="/logout">Logout</a>
+	</body>
+</html>
