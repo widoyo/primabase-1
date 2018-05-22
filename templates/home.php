5750d598b1f90cb62cf6bb046e45690c6cefa714
dhnchrs <37863926+dhnchrs@users.noreply.github.com>
2018-05-22 14:01:17 +0700

    Update home.php

--- templates/home.php
+++ templates/home.php
@@ -9,6 +9,7 @@
 		1. <a href="/formTenant">CRUD Tenant</a><br/>
 		2. <a href="/formLogger">CRUD Logger</a><br/>
 		3. <a href="/formPos">CRUD Pos</a><br/>
-		4. <a href="/logout">Logout</a>
+		4. <a href="/formPeriodic">View Periodic</a><br/>
+		5. <a href="/logout">Logout</a>
 	</body>
 </html>
